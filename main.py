9b4a92d0118e5657812370e658d1f28929876eb5
Ted Brownlow <ted.brownlow@gmail.com>
2020-05-30 12:44:16 -0400

    added language and gender flags

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('-i',dest='input')
 parser.add_argument('-o',dest='output')
+# gender is MALE,FEMALE, or NEUTRAL
+parser.add_argument('-g',dest='gender',type=str)
+# language code is en-US,en-UK,etc
+# https://cloud.google.com/text-to-speech/docs/voices
+parser.add_argument('-l',dest='language',type=str)
 args = parser.parse_args()
 
-if args.input is None or args.output is None:
-    print("Input and output need to be supplied")
+if args.input is None or args.output is None or args.gender is None or args.language is None:
+    print("MISSING ARGUMENTS")
     sys.exit()
 
 with open(args.input,'r') as input_file:
@@ -19,8 +24,8 @@ client = texttospeech.TextToSpeechClient()
 synthesis_input = texttospeech.types.SynthesisInput(text=input_str)
 
 voice = texttospeech.types.VoiceSelectionParams(
-    language_code='en-US',
-    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL
+    language_code=args.language,
+    ssml_gender=texttospeech.enums.SsmlVoiceGender[args.gender.upper()]
 )
 
 audio_config = texttospeech.types.AudioConfig(
